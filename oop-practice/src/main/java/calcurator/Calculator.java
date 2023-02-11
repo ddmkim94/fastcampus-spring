cff867eaeb6b79ade68c3b0c1f7d021a45eefd6e
ddmkim94 <ddmkim94@gmail.com>
2023-02-11 19:12:58 +0900

    Eunm 클래스 안에서 사칙 연산 기능 구현
    Calculator 클래스의 calculate 메서드는 ArithmeticOperator에게 계산을 위임한다.

--- oop-practice/src/main/java/calcurator/Calculator.java
+++ oop-practice/src/main/java/calcurator/Calculator.java
@@ -3,17 +3,6 @@ package calcurator;
 public class Calculator {
 
     public static int calculate(int operand1, String operator, int operand2) {
-        if (operator.equals("+")) {
-            return operand1 + operand2;
-        } else if (operator.equals("-")) {
-            return operand1 - operand2;
-        } else if (operator.equals("*")) {
-            return operand1 * operand2;
-        } else {
-            if (operand1 == 0) {
-                throw new IllegalStateException("0은 나눌 수 없습니다.");
-            }
-            return operand1 / operand2;
-        }
+        return ArithmeticOperator.calculate(operand1, operator, operand2);
     }
 }
