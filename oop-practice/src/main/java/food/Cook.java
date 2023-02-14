5ebb939417b264b45a88f79315a24b08141af54e
ddmkim94 <ddmkim94@gmail.com>
2023-02-14 11:16:06 +0900

    test: Cooking 객체 생성 & 테스트

--- oop-practice/src/main/java/food/Cook.java
+++ oop-practice/src/main/java/food/Cook.java
@@ -1,5 +1,7 @@
 package food;
 
+import java.util.Objects;
+
 public class Cook {
 
     private final String name;
@@ -8,5 +10,23 @@ public class Cook {
     public Cook(String name, int price) {
         this.name = name;
         this.price = price;
+    }
+
+    public Cook(MenuItem menuItem) {
+        name = menuItem.getName();
+        price = menuItem.getPrice();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cook cook = (Cook) o;
+        return price == cook.price && Objects.equals(name, cook.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
---
+++ oop-practice/src/test/java/food/CustomerTest.java
@@ -0,0 +1,11 @@
+package food;
+
+import org.junit.jupiter.api.Test;
+
+public class CustomerTest {
+
+    @Test
+    void name() throws Exception {
+
+    }
+}
