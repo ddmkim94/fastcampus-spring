5bf92b78fd9ff36a96dda5e9b477f301dfde58b8
ddmkim94 <ddmkim94@gmail.com>
2023-02-16 16:13:47 +0900

    Servlet 인터페이스 구현해서 서블릿 컨테이너 구현
    인터페이스를 구현하기 때문에 필요없는 메서드도 강제로 구현해야하는 단점을 가진다.

--- part1/servlet/src/main/java/org/example/CalculatorServlet.java
+++ part1/servlet/src/main/java/org/example/CalculatorServlet.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import org.example.calculator.Calculator;
+import org.example.calculator.calculate.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 // 서블릿 컨테이너!
 
@@ -20,20 +23,31 @@ public class CalculatorServlet implements Servlet {
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        // 서블릿 생성 후 초기화 할 때 실행
+        /**
+         * - 서블릿 생성 후 초기화 작업 수행 (최초 실행시 1번만 실행)
+         * - 다른 자원을 초기화 하는 경우에도 init()에 작성하면 된다.
+         */
         log.info("init");
     }
 
     @Override
     public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-        // 클라이언트 요청이 들어올 때마다 실행
+        /**
+         * - 클라이언트 요청이 들어올 때마다 실행되는 메서드
+         */
         log.info("service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
 
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = response.getWriter();
+        writer.println(result);
     }
 
     @Override
     public void destroy() {
-        // 서블릿이 소멸될 때 실행
         log.info("destroy");
     }
 
