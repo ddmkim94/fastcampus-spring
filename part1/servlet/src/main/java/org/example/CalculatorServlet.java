1a98095630906c43af4c80a0c644b9ca299e4926
ddmkim94 <ddmkim94@gmail.com>
2023-02-16 16:47:16 +0900

    HttpServlet을 상속하도록 리팩토링

--- part1/servlet/src/main/java/org/example/CalculatorServlet.java
+++ part1/servlet/src/main/java/org/example/CalculatorServlet.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -17,12 +20,12 @@ import java.io.PrintWriter;
  * 서블릿 객체를 싱글톤으로 관리한다.
  */
 @WebServlet("/calculate")
-public class CalculatorServlet extends GenericServlet {
+public class CalculatorServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
 
     @Override
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         /**
          * - 클라이언트 요청이 들어올 때마다 실행되는 메서드
          */
