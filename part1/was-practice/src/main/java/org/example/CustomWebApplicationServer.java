874b19d3af81a321e8b9541497b8f203c6c6796b
ddmkim94 <ddmkim94@gmail.com>
2023-02-16 14:26:43 +0900

    Thread Pool을 이용해서 좀 더 효율적으로 동작하도록 수정

--- part1/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ part1/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -9,10 +9,15 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
 
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
@@ -32,8 +37,7 @@ public class CustomWebApplicationServer {
                 /**
                  * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리한다.
                  */
-                new Thread(new ClientRequestHandler(clientSocket)).start();
-
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
