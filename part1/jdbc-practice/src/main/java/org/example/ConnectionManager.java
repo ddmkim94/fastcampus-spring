d447719b9e1216b71c33b0f9f515bafe4eef3b19
ddmkim94 <ddmkim94@gmail.com>
2023-02-17 17:02:59 +0900

    ConnectionManager 리팩토링
    데이터베이스 연결과 관련된 코드를 ConnectionManager에서 관리하도록 수정
    HikariCP를 사용해서 데이터 커넥션 풀 적용

--- part1/jdbc-practice/src/main/java/org/example/ConnectionManager.java
+++ part1/jdbc-practice/src/main/java/org/example/ConnectionManager.java
@@ -3,16 +3,40 @@ package org.example;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    public static DataSource getDataSource() {
-        HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=1");
-        hikariDataSource.setUsername("sa");
-        hikariDataSource.setPassword("");
+    public static final String DB_DRIVER = "org.h2.Driver";
+    public static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=1";
+    public static final String DB_USERNAME = "sa";
+    public static final String DB_PASSWORD = "";
+    public static final int MAX_POOL_SIZE = 40;
 
-        return hikariDataSource;
+    private static final DataSource ds;
+
+    static { // ds는 한개만 존재하기 때문에 클래스당 1회만 실행되도록 static 초기화
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
+        hikariDataSource.setUsername(DB_USERNAME);
+        hikariDataSource.setPassword(DB_PASSWORD);
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE); // 최대 커넥션 개수 == 커넥션 풀 크기
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
+
+        ds = hikariDataSource;
+    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static DataSource getDataSource() {
+        return ds;
     }
 }
--- part1/jdbc-practice/src/main/java/org/example/UserDao.java
+++ part1/jdbc-practice/src/main/java/org/example/UserDao.java
@@ -3,26 +3,12 @@ package org.example;
 import java.sql.*;
 
 public class UserDao {
-
-    private Connection getConnection() {
-        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=1";
-        String username = "sa";
-        String password = "";
-
-        try {
-            Class.forName("org.h2.Driver");
-            return DriverManager.getConnection(url, username, password);
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
     public void create(User user) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
 
         try {
-            con = getConnection();
+            con = ConnectionManager.getConnection();
 
             String sql = "INSERT INTO USERS VALUES(?, ?, ?, ?)";
             pstmt = con.prepareStatement(sql);
@@ -53,7 +39,7 @@ public class UserDao {
         ResultSet rs = null;
 
         try {
-            con = getConnection();
+            con = ConnectionManager.getConnection();
 
             String sql = "SELECT * FROM USERS WHERE userId = ?";
             pstmt = con.prepareStatement(sql);
