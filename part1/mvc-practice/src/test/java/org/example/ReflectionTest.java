565195d73ece2d98a6e4c44c21e7fff8fe9ce918
ddmkim94 <ddmkim94@gmail.com>
2023-02-18 16:52:16 +0900

    리플렉션 API로 User 클래스의 정보 가져오기

--- part1/mvc-practice/src/test/java/org/example/ReflectionTest.java
+++ part1/mvc-practice/src/test/java/org/example/ReflectionTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.annotation.Controller;
 import org.example.annotation.Service;
+import org.example.model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
@@ -9,9 +10,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @Controller 애노테이션이 설정되어 있는 모든 클래스를 찾아서 출력한다.
@@ -26,6 +29,16 @@ public class ReflectionTest {
         Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
 
         log.debug("beans: [{}]", beans);
+    }
+
+    @Test
+    void showClass() {
+        Class<User> clazz = User.class;
+
+        log.debug("클래스 이름 = {}", clazz.getName());
+        log.debug("User all declared fields = [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+        log.debug("User all declared constructors = [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
+        log.debug("User all declared methods = [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
     }
 
     // 파라미터로 Annotation 타입의 클래스만 올 수 있다.
