583e6164fa3ffa24e00b6d28e66671418660701d
ddmkim94 <ddmkim94@gmail.com>
2023-02-18 15:43:55 +0900

    JdbcTemplate 리팩토링

--- part1/jdbc-practice/src/main/java/org/example/JdbcTemplate.java
+++ part1/jdbc-practice/src/main/java/org/example/JdbcTemplate.java
@@ -2,11 +2,12 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcTemplate {
 
-    public void executeUpdate(User user, String sql, PreparedStatementSetter pss) throws SQLException {
+    public void executeUpdate(String sql, PreparedStatementSetter pss) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
 
@@ -17,6 +18,38 @@ public class JdbcTemplate {
 
             pstmt.executeUpdate();
         } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    // 특정 회원 조회
+    public Object executeQuery(String sql, PreparedStatementSetter pss, RowMapper mapper) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+            pss.setter(pstmt);
+
+            rs = pstmt.executeQuery();
+
+            Object obj = null;
+            while (rs.next()) {
+                obj = mapper.map(rs);
+            }
+
+            return obj;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
             if (pstmt != null) {
                 pstmt.close();
             }
--- part1/jdbc-practice/src/main/java/org/example/UserDao.java
+++ part1/jdbc-practice/src/main/java/org/example/UserDao.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 import java.sql.*;
 
 public class UserDao {
@@ -8,7 +7,7 @@ public class UserDao {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
         String sql = "INSERT INTO USERS VALUES(?, ?, ?, ?)";
-        jdbcTemplate.executeUpdate(user, sql, pstmt -> {
+        jdbcTemplate.executeUpdate(sql, pstmt -> {
             pstmt.setString(1, user.getUserId());
             pstmt.setString(2, user.getPassword());
             pstmt.setString(3, user.getName());
@@ -18,43 +17,16 @@ public class UserDao {
 
     // 특정 회원 조회
     public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "SELECT * FROM USERS WHERE userId = ?";
 
-        try {
-            con = ConnectionManager.getConnection();
-
-            String sql = "SELECT * FROM USERS WHERE userId = ?";
-            pstmt = con.prepareStatement(sql);
-
-            pstmt.setString(1, userId);
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            while (rs.next()) {
-                user = new User(
+        return (User) jdbcTemplate.executeQuery(sql,
+                pstmt -> pstmt.setString(1, userId),
+                rs -> new User(
                         rs.getString("userId"),
                         rs.getString("password"),
                         rs.getString("name"),
                         rs.getString("email")
-                );
-            }
-
-            return user;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
-        return null;
+                ));
     }
 }
