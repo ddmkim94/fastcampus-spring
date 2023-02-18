7bee4d2aed0bac44a58f45a34ff23bf3d51cb63a
ddmkim94 <ddmkim94@gmail.com>
2023-02-18 16:41:44 +0900

    리플렉션 API로 @Controller, @Service가 붙은 클래스 조회

--- part1/mvc-practice/src/test/java/org/example/ReflectionTest.java
+++ part1/mvc-practice/src/test/java/org/example/ReflectionTest.java
@@ -8,6 +8,9 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -20,10 +23,17 @@ public class ReflectionTest {
     @DisplayName("@Controller 애노테이션이 설정된 클래스 찾기!")
     @Test
     void controllerScan() {
-        Reflections reflections = new Reflections("org.example"); // org.example 하위 패키지에 있는 모든 클래스를 대상으로 조회
-        // Controller 애노테이션을 가진 클래스를 조회한다!
-        Set<Class<?>> beans = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
 
         log.debug("beans: [{}]", beans);
+    }
+
+    // 파라미터로 Annotation 타입의 클래스만 올 수 있다.
+    private Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
+        Reflections reflections = new Reflections("org.example"); // org.example 하위 패키지에 있는 모든 클래스를 대상으로 조회
+        // Controller 애노테이션을 가진 클래스를 조회한다!
+        Set<Class<?>> beans = new HashSet<>();
+        annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
+        return beans;
     }
 }
