29ea89bb5e2fc212d46cbee7da157c98ad6fa25c
ddmkim94 <ddmkim94@gmail.com>
2023-02-20 14:37:53 +0900

    forward 요청을 처리하는 JspView 구현

---
+++ part1/mvc-framework-practice/src/main/java/org/example/mvc/view/JspView.java
@@ -0,0 +1,25 @@
+package org.example.mvc.view;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class JspView implements View{
+
+    private final String name;
+
+    public JspView(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        model.forEach(request::setAttribute);
+
+        RequestDispatcher rd = request.getRequestDispatcher(name);
+        rd.forward(request, response);
+    }
+}
