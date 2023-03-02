15eec7a3d071758a99500b59623a7a36f087597a
ddmkim94 <ddmkim94@gmail.com>
2023-03-02 13:57:01 +0900

    #27 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- part2/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ part2/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -22,7 +22,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현중..")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void articleListViewTest() throws Exception {
@@ -35,7 +34,6 @@ class ArticleControllerTest {
 
     }
 
-    @Disabled("구현중..")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     void articleDetailViewTest() throws Exception {
