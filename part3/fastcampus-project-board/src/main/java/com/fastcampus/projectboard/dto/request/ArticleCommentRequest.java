15fbe908f112f7a49b24b8458018d0065df6062d
ddmkim94 <ddmkim94@gmail.com>
2023-03-14 22:45:20 +0900

    #78 - 대댓글 비즈니스 로직 구현
    부모/자식 관계를 만들면서 entity <-> dto 간 변환 로직,
    댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법,
    댓글과 대댓글을 db 데이터로부터 변환할 때
    서로 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- part3/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/request/ArticleCommentRequest.java
+++ part3/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/request/ArticleCommentRequest.java
@@ -3,16 +3,26 @@ package com.fastcampus.projectboard.dto.request;
 import com.fastcampus.projectboard.dto.ArticleCommentDto;
 import com.fastcampus.projectboard.dto.UserAccountDto;
 
-public record ArticleCommentRequest(Long articleId, String content) {
+public record ArticleCommentRequest(
+        Long articleId,
+        Long parentCommentId,
+        String content
+) {
 
+    // 그대로 놔두는 것이 변경을 최소화 할 수 있는 방법
     public static ArticleCommentRequest of(Long articleId, String content) {
-        return new ArticleCommentRequest(articleId, content);
+        return ArticleCommentRequest.of(articleId, null, content);
+    }
+
+    public static ArticleCommentRequest of(Long articleId, Long parentCommentId, String content) {
+        return new ArticleCommentRequest(articleId, parentCommentId, content);
     }
 
     public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
         return ArticleCommentDto.of(
                 articleId,
                 userAccountDto,
+                parentCommentId,
                 content
         );
     }
