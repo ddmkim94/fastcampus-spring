9b3b27dda7fa38df9efb410ce932ab78b2941f8f
ddmkim94 <ddmkim94@gmail.com>
2023-03-14 17:40:58 +0900

    #76 - 대댓글 도메인 설계
    대댓글 도메인 안에서 부모, 자식 관계를 설정하는 코드를 추가
    자식 댓글의 컬렉션 변화가 쿼리에 반영되게끔
    cascading 규칙을 모두 적용
    이번엔 단방향 연관관계 설정을 사용해보기로 함
    따라서 부모 댓글은 엔티티가 아닌 `Long` id를 직접 표현
    
    또한 자식 댓글을 추가할 수 있는 메소드 추가 제공

--- part3/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ part3/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -30,19 +32,39 @@ public class ArticleComment extends AuditingFields {
     private UserAccount userAccount; // 유저 정보 (ID)
 
     @Setter
+    @Column(updatable = false)
+    private Long parentCommentId;
+
+    // 부모 댓글이 지워지면 자식 댓글도 함께 삭제
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
+    @Setter
     @Column(nullable = false, length = 500)
     private String content; // 본문
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, UserAccount userAccount,String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
+    /**
+     * 연결을 끊는 것의 장점은 도메인 코드의 변경에 대한 전파를 최소화 할 수 있다는 점이다.
+     * 도메인과 연관된 곳의 코드 변경이 최소화
+      */
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
